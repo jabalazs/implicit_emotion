ca5cd13dd9706cae0a3de1da9db94c6a94cfee78
Jorge Balazs <jabalazst@gmail.com>
2018-05-31 11:49:43 +0900

    Add emoji dict, improve preprocessing script

--- preprocess.sh
+++ preprocess.sh
@@ -6,44 +6,50 @@ TRAIN_DATA_PATH="data/train.csv"
 DEV_DATA_PATH="data/trial.csv"
 
 CLEAN_TRAIN_DATA_PATH="$PREPROCESSED_DATA_DIR/train.csv"
-CLEAN_TRAIN_NO_EMOJIS_DATA_PATH="$PREPROCESSED_DATA_DIR/train_no_emojis.csv"
 TRAIN_LABELS_PATH="$PREPROCESSED_DATA_DIR/train_labels.csv"
 CLEAN_DEV_DATA_PATH="$PREPROCESSED_DATA_DIR/dev.csv"
+
+CLEAN_TRAIN_NO_EMOJIS_DATA_PATH="$PREPROCESSED_DATA_DIR/train_no_emojis.csv"
+CLEAN_DEV_NO_EMOJIS_DATA_PATH="$PREPROCESSED_DATA_DIR/dev_no_emojis.csv"
+
 
 mkdir $PREPROCESSED_DATA_DIR
 echo "Created dir $PREPROCESSED_DATA_DIR"
 
+echo "Copying dev labels"
 cp -v data/trial.labels $PREPROCESSED_DATA_DIR/dev_labels.csv
 
+echo
 
 echo "Preprocessing $TRAIN_DATA_PATH"
-cat $TRAIN_DATA_PATH | awk '{$1=""; gsub("\\[#TRIGGERWORD#\\]", "__TRIGGERWORD__", $0); 
-                                  gsub("@USERNAME", "__USERNAME__", $0); 
-                                  gsub("\\[NEWLINE\\]", "__NEWLINE__", $0); 
-                                  gsub("http://url.removed", "__URL__", $0); print $0}' \
+cat $TRAIN_DATA_PATH | awk '{$1=""; gsub("\\[#TRIGGERWORD#\\]", " __TRIGGERWORD__ ", $0); 
+                                  gsub("@USERNAME", " __USERNAME__ ", $0); 
+                                  gsub("\\[NEWLINE\\]", " __NEWLINE__ ", $0); 
+                                  gsub("http://url.removed", " __URL__ ", $0); print $0}' \
                    | ./twokenize.py > $CLEAN_TRAIN_DATA_PATH
 
 cat $TRAIN_DATA_PATH | awk '{print $1}' > $TRAIN_LABELS_PATH
 
 echo "Created $CLEAN_TRAIN_DATA_PATH and $TRAIN_LABELS_PATH"
 
+echo
 
 echo "Preprocessing $DEV_DATA_PATH"
-cat $DEV_DATA_PATH | awk '{$1=""; gsub("\\[#TRIGGERWORD#\\]", "__TRIGGERWORD__", $0); 
-                                  gsub("@USERNAME", "__USERNAME__", $0); 
+cat $DEV_DATA_PATH | awk '{$1=""; gsub("\\[#TRIGGERWORD#\\]", " __TRIGGERWORD__ ", $0); 
+                                  gsub("@USERNAME", " __USERNAME__ ", $0); 
                                   gsub("\\[NEWLINE\\]", " __NEWLINE__ ", $0); 
-                                  gsub("http://url.removed", "__URL__", $0); print $0}' \
+                                  gsub("http://url.removed", " __URL__ ", $0); print $0}' \
                    | ./twokenize.py > $CLEAN_DEV_DATA_PATH
 echo "Created $CLEAN_DEV_DATA_PATH"
 
-# remove unicode markers and some emojis that contain ascii symbols
-# (beginning with the keycap symbol) and were 
-# making the script remove hashtags
-cat data/unicode_codes.py | awk '{if ( $1 !~ /keycap/ )
-                                    {gsub("u'\'':", "'\'':", $1);
-                                     gsub("u'\''", "'\''", $2); print $0}
-                                 }' > unicode_codes_py3.py
+echo
 
-echo "Removing emojis"
+echo "Removing emojis from train"
 cat $CLEAN_TRAIN_DATA_PATH | ./remove_emojis.py > $CLEAN_TRAIN_NO_EMOJIS_DATA_PATH
 echo "Created $CLEAN_TRAIN_NO_EMOJIS_DATA_PATH"
+
+echo
+
+echo "Removing emojis from dev"
+cat $CLEAN_DEV_DATA_PATH | ./remove_emojis.py > $CLEAN_DEV_NO_EMOJIS_DATA_PATH
+echo "Created $CLEAN_DEV_NO_EMOJIS_DATA_PATH"
