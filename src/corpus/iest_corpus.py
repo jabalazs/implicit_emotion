90ec7044e5f2970ab84d35c6a634fc8a90a516b5
Jorge Balazs <jabalazst@gmail.com>
2018-06-07 10:59:13 +0900

    Finish refactoring Corpus object creation

--- src/corpus/iest_corpus.py
+++ src/corpus/iest_corpus.py
@@ -52,66 +52,10 @@ class IESTCorpus(BaseCorpus):
                                    self.train_sents,
                                    force_reload=self.force_reload)
 
-        train_pickle_path = os.path.join(config.CACHE_PATH, 'train.pkl')
-        self.train_id_sents = load_or_create(train_pickle_path,
-                                             self.lang.sents2ids,
-                                             self.train_sents,
-                                             force_reload=self.force_reload)
-
-        self.dev_id_sents = self.lang.sents2ids(self.dev_sents)
-
-        train_chars_pickle_path = os.path.join(config.CACHE_PATH,
-                                               'train_chars.pkl')
-        self.train_char_id_sents = load_or_create(
-                               train_chars_pickle_path,
-                               self.lang.sents2char_ids,
-                               self.train_sents,
-                               force_reload=self.force_reload)
-
-        dev_chars_pickle_path = os.path.join(config.CACHE_PATH,
-                                             'dev_chars.pkl')
-        self.dev_char_id_sents = load_or_create(
-                               dev_chars_pickle_path,
-                               self.lang.sents2char_ids,
-                               self.dev_sents,
-                               force_reload=self.force_reload)
-
         self.label2id = {key: value for key, value in config.LABEL2ID.items()}
 
-        train_labels = open(config.LABEL_PATHS['train']).readlines()
-        self.train_labels = [l.rstrip() for l in train_labels]
-        self.train_id_labels = [self.label2id[label]
-                                for label in self.train_labels]
-
-        dev_labels = open(config.LABEL_PATHS['dev']).readlines()
-        self.dev_labels = [l.rstrip() for l in dev_labels]
-        self.dev_id_labels = [self.label2id[label]
-                              for label in self.dev_labels]
-
-        self.train_ids = range(len(self.train_id_sents))
-        self.dev_ids = range(len(self.dev_id_sents))
-
-        train_examples = zip(self.train_ids,
-                             self.train_id_sents,
-                             self.train_char_id_sents,
-                             self.train_id_labels)
-
-        dev_examples = zip(self.dev_ids,
-                           self.dev_id_sents,
-                           self.dev_char_id_sents,
-                           self.dev_id_labels)
-
-        self.train_examples = [{'id': ex[0],
-                                'sequence': ex[1],
-                                'char_sequence': ex[2],
-                                'label': ex[3]} for ex in train_examples]
-
-        import ipdb; ipdb.set_trace(context=10)
-
-        self.dev_examples = [{'id': ex[0],
-                              'sequence': ex[1],
-                              'char_sequence': ex[2],
-                              'label': ex[3]} for ex in dev_examples]
+        self.train_examples = self._create_examples(self.train_sents, mode='train')
+        self.dev_examples = self._create_examples(self.dev_sents, mode='dev')
 
         self.train_batches = BatchIterator(self.train_examples,
                                            self.batch_size,
@@ -127,11 +71,15 @@ class IESTCorpus(BaseCorpus):
                                          batch_first=self.batch_first,
                                          use_chars=self.use_chars)
 
-    def create_examples(self, sents, mode):
+    def _create_examples(self, sents, mode):
         """
         sents: list of strings
         mode: (string) train, dev or test
+
+        return:
+            examples: a list containing dicts representing each example
         """
+
         allowed_modes = ['train', 'dev', 'test']
         if mode not in allowed_modes:
             raise ValueError(f'Mode not recognized, try one of {allowed_modes}')
